23ee35a19436f2d9a38f35d7b440d0d7b6433534
asus <asus@LAPTOP-QK8GBRF9>
2024-07-12 23:55:42 -0700

    Add project files.

---
+++ output/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace output
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("I like pizza!");
+                Console.WriteLine("It's really good!");
+            }
+
+        }
+    }
+
+}
