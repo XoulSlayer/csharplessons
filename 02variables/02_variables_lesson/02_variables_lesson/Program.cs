09563785757b95020bbec17d6ca93e9ed4fa8cbe
XoulSlayer <149201490+XoulSlayer@users.noreply.github.com>
2024-07-13 21:31:23 -0700

    Add files via upload

---
+++ 02variables/02_variables_lesson/02_variables_lesson/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace output
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            {
+                //ssay i want picca vgtkjgktgh4gh4kg
+                /*
+                 * evve
+                 * eve
+                 * 
+                 * evveve
+                 * 
+                 * ev
+                 */
+
+                int nathan_age = 5;
+                int aali_age = 16;
+                int asha_age = 21;
+
+                string nathan_name = "nathan";
+                string aali_name = "aali";
+                string asha_name = "asha";
+
+                double nathan_weight = 150.5;
+               
+                nathan_age = 24;
+                bool of_age;
+                of_age = false;
+
+
+                Console.WriteLine("NAME: " + nathan_name + "   AGE: " + nathan_age + "   WEIGHT: " + nathan_weight + " Of age?: "+  of_age) ; ;
+                Console.WriteLine("NAME: " + aali_name + "   AGE: " + aali_age);
+                Console.WriteLine("NAME: " + asha_name + "    AGE: " + asha_age);
+
+
+
+
+
+
+
+                Console.ReadKey();
+            }
+
+        }
+    }
+
+}
