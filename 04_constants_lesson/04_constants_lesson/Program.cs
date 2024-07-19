8beee6d5d06cc708ffff1929e04cae90aae920af
XoulSlayer <149201490+XoulSlayer@users.noreply.github.com>
2024-07-19 13:19:26 -0700

    Add files via upload

---
+++ 04_constants_lesson/04_constants_lesson/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace constants
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constans = immutable values which are known at compile time 
+            //           and do not change for the life of the program
+
+            const double pi = 3.14159;
+
+            Console.WriteLine(pi);
+
+            Console.ReadKey();
+        }
+    }
+}
